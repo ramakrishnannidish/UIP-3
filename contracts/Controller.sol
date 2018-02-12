03a0dd4965872a6252381596efb355c199a6310a
Nidish Ramakrishnan <ramakrishnannidish@gmail.com>
2018-02-12 17:51:48 +0530

    Feat: Cap logic to token

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,12 +1,24 @@
 pragma solidity ^0.4.11;
 
-import "./MintableToken.sol";
+import "./CappedToken.sol";
 
 
-contract Controller is MintableToken {
+contract Controller is CappedToken {
 
   string public name = "Upgradeable Token";
   string public symbol = "UPT";
   uint8 public decimals = 18;
 
+  function Controller(uint256 _cap) public
+    CappedToken(_cap)
+  {
+
+  }
+
+  function initialize(uint256 _cap) public {
+    require(cap == 0);
+    require(_cap > 0);
+    cap = _cap;
+    totalSupply_ = 0;
+  }
 }
