b98fad01a639c62cb7f31f32be566bc735214139
Nidish Ramakrishnan <ramakrishnannidish@gmail.com>
2018-02-12 20:17:04 +0530

    Added constant tags to hardcode getter

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -5,9 +5,9 @@ import "./CappedToken.sol";
 
 contract Controller is CappedToken {
 
-  string public name = "Upgradeable Token";
-  string public symbol = "UPT";
-  uint8 public decimals = 18;
+  string public constant name = "Upgradeable Token";
+  string public constant symbol = "UPT";
+  uint8 public constant decimals = 18;
 
   function Controller(uint256 _cap) public
     CappedToken(_cap)
